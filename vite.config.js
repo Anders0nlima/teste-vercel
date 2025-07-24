7423d41f1195fa54e81bd577dcfac22ecbef2a11
Anders0nlima <andersoncon85@gmail.com>
2025-07-24 14:55:53 -0300

    feat: preparar projeto para deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Detecta se está publicando no GitHub Pages
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/teste-vercel"
+  base: isGitHubPages ? '/teste-vercel/' : '/', // Para GitHub usa /teste-vercel/, para Vercel usa /
 })
